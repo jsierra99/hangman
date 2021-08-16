c9c5326b4f16ee2a9e08ae5de6a7733653d7de79
Jay Sierra <jsierra99@live.com>
2021-08-15 21:50:33 -0700

    hangman

---
+++ js/modules/end.js
@@ -0,0 +1,36 @@
+const End = (_ =>
+{
+    const state =
+    {
+        chosenWord: null,
+        winOrLose: null
+    }
+
+    const $hangman = document.querySelector(".hangman");
+
+    const setState = x =>
+    {
+        state.chosenWord = x.chosenWord;
+        state.winOrLose = x.result;
+        render();
+    }
+
+    const render = _ =>
+    {
+        let markup =
+        `
+            <h1 class="hangman__title">GAME OVER</h1>
+            <p class="result">You ${state.winOrLose.toUpperCase()}! <br>
+            The word is > <span class="lime">${state.chosenWord.toUpperCase()}!</span></p>
+            <button class="button hangman__trigger">Main Menu</button>
+        `
+
+        $hangman.innerHTML = markup;
+    }
+
+    return {
+        setState
+    };
+})();
+
+export default End;
